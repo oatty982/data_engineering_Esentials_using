79cf6cc40a98e785ced99f50ce45220a2df5b926
Patcharapon Chaiporn <oatpatcharapon01@hotmail.com>
2023-06-22 01:54:09 +0700

    main

--- python/5 file_format_converter/setup_project.bash
+++ python/5 file_format_converter/setup_project.bash
@@ -6,7 +6,12 @@ ls ./ffc-venv/bin/
 
 ls ./file_format_converter/ffc-venv/bin/
 
-source file_format_converter/ffc-venv/bin/activate
+source python/5\ file_format_converter/ffc-venv/bin/activate
+
+telnet 5432
+Trying 0.0.21.56...
+telnet: connect to address 0.0.21.56: No route to host
+telnet: Unable to connect to remote host
 
 python app.py 
 
